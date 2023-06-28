46874c5e86c4da0b980dc090416911e6aa7cc983
tsuyuni <tsuyu.lit@gmail.com>
2023-06-28 15:12:14 +0900

    Initial Commit

---
+++ CountTimerSwiftUI/CountTimerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountTimerSwiftUIApp.swift
+//  CountTimerSwiftUI
+//
+//  Created by Yuri Tsuchikawa on 2023/06/28.
+//
+
+import SwiftUI
+
+@main
+struct CountTimerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
